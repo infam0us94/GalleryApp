8d4b374e20c452d98b99cf5794665ce67ffa9bb9
infam0us94 <ivanpopoff94@mail.ru>
2021-02-28 22:09:22 +0300

    search view

--- app/src/main/java/com/project/imageapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/project/imageapp/ui/gallery/GalleryFragment.kt
@@ -1,21 +1,68 @@
 package com.project.imageapp.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.project.imageapp.R
+import com.project.imageapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
-
+        _binding = FragmentGalleryBinding.bind(view)
+        val adapter = UnsplashPhotoAdapter()
+        binding.apply {
+            recView.setHasFixedSize(true)
+            recView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashPhotoLoadAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadAdapter { adapter.retry() }
+            )
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.recView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
